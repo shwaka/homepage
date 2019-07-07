3444882dfc66afb5df8276f645d65d5831ae8e3c
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2019-07-08 01:26:09 +0900

    modify

--- old/KS/tweet.py
+++ old/KS/tweet.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumer_key = "NMmC4LNUneGCjfzTpT7AzTDWo"
-consumer_secret = "bdoiUaGhIqbM8MEoa987KZINY6mpOQgkLFDxghXkSaKesyI94l"
-access_token = "3236648762-Agk1xJkEwNDSoNGX1MmNNPslVkDMbifx0gvsH1Z"
-access_secret = "73HhyzHBlBchPxbzJFeJojbnwfXn34PHrOIF9glG3SrcT"
+consumer_key = "..."
+consumer_secret = "..."
+access_token = "..."
+access_secret = "..."
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
