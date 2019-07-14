4e2c6662956da87b20d816d7b8582f3989cd1666
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2019-07-14 16:50:41 +0900

    convert md to html in include_md_tag.rb

--- jekyll/_plugins/include_md_tag.rb
+++ jekyll/_plugins/include_md_tag.rb
@@ -23,7 +23,13 @@ module Jekyll
           :strict_filters => liquid_options["strict_filters"],
           :strict_variables => liquid_options["strict_variables"],
         }
-        return template.render!(context, info)
+        rendered_md = template.render!(context, info)
+        # return rendered_md
+        # convert markdown to html
+        # converter = site.converters[0]
+        converter = site.converters.find{|conv| conv.matches(".md")}
+        puts converter
+        return converter.convert(rendered_md)
       else
         raise "not found: page or post with ref=#{@ref}, lang=#{lang}"
       end
