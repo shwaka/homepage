868cf9bb0fbe971dfafb973991e7c66404a680d6
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2019-07-15 00:02:00 +0900

    writing refer_filter.rb

---
+++ jekyll/_plugins/refer_filter.rb
@@ -0,0 +1,52 @@
+module Jekyll
+  module MyModule
+    def alist_to_hash(alist)
+      # alist = association list (from lisp)
+      if alist.length % 2 != 0 then
+        raise "alist.length must be even!"
+      end
+      hash_result = {}
+      (0...(alist.length / 2)).each do |i|
+        hash_result[alist[2*i]] = alist[2*i+1]
+      end
+      return hash_result
+    end
+    def find_page(pages, refer_key, ref, options)
+      pages_filtered = pages
+                         .select{|page| options.keys.all?{|key| page.data[key] == options[key]}}
+                         .select{|page| page.data[refer_key] == ref }
+      if pages_filtered.length == 0 then
+        return nil
+      elsif pages_filtered.length == 1 then
+        return pages_filtered[0]
+      else
+        raise "too many pages matched"
+      end
+    end
+    module_function :alist_to_hash, :find_page
+    module MyFilter
+      def refer(input, *args)
+        site = @context.registers[:site]
+        refer_key = site.config["refer"]["key"]
+        default_value = site.config["refer"]["default_value"]
+        options = MyModule.alist_to_hash(args)
+        pages = site.pages + site.posts.docs
+        page = MyModule.find_page(pages, refer_key, input, options) ||
+               MyModule.find_page(pages, refer_key, default_value, options)
+        return page
+      end
+      def refer_url(input)
+        # input must be a page
+        return input["url"]
+      end
+      def refer_link(input)
+        # input must be a page
+        # url = Jekyll::Filters::URLFilters.absolute_url(refer_url(input))
+        url = refer_url(input)
+        return "hoge: #{url}"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::MyModule::MyFilter)
