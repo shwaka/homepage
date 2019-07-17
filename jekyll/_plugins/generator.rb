a4991554eb634aa9f2364c6792b7da959fe45fbe
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2019-07-17 21:37:42 +0900

    change dark-mode to palette-lightness

--- jekyll/_plugins/generator.rb
+++ jekyll/_plugins/generator.rb
@@ -1,27 +1,27 @@
 module Jekyll
   class HelloWorldPage < Page
-    def initialize(site, base, dir, name, color, dark_mode)
+    def initialize(site, base, dir, name, color, lightness)
       @site, @base, @dir, @name = site, base, dir, name
       self.process(@name)
       self.read_yaml(File.join(@base, "assets/css"), "template.scss")
       self.data["base-color"] = color
-      self.data["dark-mode"] = dark_mode
+      self.data["palette-lightness"] = lightness
     end
   end
   class TestGenerator < Generator
     safe true
     def generate(site)
       n = site.config["num_styles"]
-      [true, false].each do |dark_mode|
+      ["light", "dark"].each do |lightness|
         (0...n).each do |i|
-          if dark_mode
-            name = "style#{i}-dark.scss"
-          else
+          if lightness == "light"
             name = "style#{i}.scss"
+          elsif lightness == "dark"
+            name = "style#{i}-dark.scss"
           end
           hue = 360 * i / n
           color = "hsl(#{hue}, 30%, 50%)"
-          site.pages << HelloWorldPage.new(site, site.source, "assets/css", name, color, dark_mode)
+          site.pages << HelloWorldPage.new(site, site.source, "assets/css", name, color, lightness)
         end
       end
     end
