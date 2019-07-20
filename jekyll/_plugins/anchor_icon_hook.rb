4646bc61c9360dc981fe4ffbfbf21f245a2674d4
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2019-07-20 18:57:00 +0900

    use svg icon in target=_blank

---
+++ jekyll/_plugins/anchor_icon_hook.rb
@@ -0,0 +1,33 @@
+# coding: utf-8
+require "nokogiri"
+
+def insert_anchor_icon(page)
+  if not [".md", ".html"].include?(page.extname)
+    return
+  end
+  html_str = page.output
+  doc = Nokogiri::HTML.parse(html_str, nil, "utf-8")
+  doc.css('a[target="_blank"]').each do |a|
+    # create <use> tag
+    use = Nokogiri::XML::Node.new("use", a)
+    baseurl = page.site.config["baseurl"]
+    use["xlink:href"] = "#{baseurl}/assets/img/icons.svg#target_blank"
+    # create <svg> tag
+    svg = Nokogiri::XML::Node.new("svg",a)
+    svg["class"] = "target-blank-icon"
+    # svg["width"] = "13px"
+    # svg["height"] = "13px"
+    svg << use
+    # edit <a> tag
+    a << svg
+  end
+  page.output = doc.to_html
+end
+
+Jekyll::Hooks.register :pages, :post_render do |page|
+  insert_anchor_icon(page)
+end
+
+Jekyll::Hooks.register :posts, :post_render do |page|
+  insert_anchor_icon(page)
+end
