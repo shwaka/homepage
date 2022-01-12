4f381fd68005561323855d2ae3a07de520c6e834
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-01-12 20:11:28 +0900

    remove unused files

---
+++ docusaurus/docs/dummy.md
@@ -0,0 +1,1 @@
+Existence of a markdown file in `docs/` is required by docusaurus.
--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -21,11 +21,11 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        // docs: {
+        //   sidebarPath: require.resolve('./sidebars.js'),
+        //   // Please change this to your repo.
+        //   editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
