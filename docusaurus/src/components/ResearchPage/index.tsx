068424e26e9a0c0045d3f24a7d36f3a6cea6a294
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-01-26 19:24:15 +0900

    improve talks.tsx

--- docusaurus/src/components/ResearchPage/index.tsx
+++ docusaurus/src/components/ResearchPage/index.tsx
@@ -1,10 +1,12 @@
 import { articles } from "@data/articles"
+import { Locale, useLocale } from "@data/locale"
 import { talks } from "@data/talks"
 import React from "react"
 import { getArticleLi } from "./articles"
 import { getTalkLi } from "./talks"
 
 export function ResearchPage(): JSX.Element {
+  const locale: Locale = useLocale()
   return (
     <>
       <h2>Articles</h2>
@@ -13,7 +15,7 @@ export function ResearchPage(): JSX.Element {
       </ul>
       <h2>Talks</h2>
       <ul>
-        {talks.map(getTalkLi)}
+        {talks.map((talk, index) => getTalkLi(talk, index, locale))}
       </ul>
     </>
   )
--- docusaurus/src/components/ResearchPage/talks.tsx
+++ docusaurus/src/components/ResearchPage/talks.tsx
@@ -1,16 +1,37 @@
-import { TalkObject } from "@data/talks"
+import { Locale, locales } from "@data/locale"
+import { TalkObject, TalkInfo } from "@data/talks"
 import { md, Markdown } from "@data/util"
 import React from "react"
 import { HtmlFromMarkdown } from "../HtmlFromMarkdown"
 
-function getMarkdown(talk: TalkObject): Markdown {
-  return md(`Given in ${talk.base.lang}`)
+function getTalkInfo(talk: TalkObject, locale: Locale): TalkInfo {
+  const localeList: Locale[] = [locale].concat(locales) // 現在の locale を優先的に表示する
+  for (const l of localeList) {
+    const talkInfo: TalkInfo | undefined = talk[l]
+    if (talkInfo !== undefined) {
+      return talkInfo
+    }
+  }
+  throw new Error("Invalid data!")
 }
 
-export function getTalkLi(talk: TalkObject, index: number): JSX.Element {
+function getConferenceLink(talkInfo: TalkInfo): string {
+  if (talkInfo.url !== undefined) {
+    return `[${talkInfo.conference}](${talkInfo.url})`
+  } else {
+    return talkInfo.conference
+  }
+}
+
+function getMarkdown(talk: TalkObject, locale: Locale): Markdown {
+  const talkInfo: TalkInfo = getTalkInfo(talk, locale)
+  return md(`${talkInfo.title}, ${getConferenceLink(talkInfo)}, ${talkInfo.venue}, ${talk.base.date}`)
+}
+
+export function getTalkLi(talk: TalkObject, index: number, locale: Locale): JSX.Element {
   return (
     <li key={index}>
-      <HtmlFromMarkdown markdown={getMarkdown(talk)}/>
+      <HtmlFromMarkdown markdown={getMarkdown(talk, locale)}/>
     </li>
   )
 }
