8d2cdcca3a0c5b0e9f2cdaf43faed954e18784be
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-01-26 16:58:05 +0900

    add talks.ts (just convert YAML to JSON)

---
+++ docusaurus/src/data/talks.ts
@@ -0,0 +1,324 @@
+export const talks = [
+  // {
+  //   "base": {
+  //     "date": "2014-12-13T00:00:00.000Z",
+  //     "lang": "ja"
+  //   },
+  //   "ja": {
+  //     "title": "Thompson群とループ空間のコホモロジー",
+  //     "conference": "無限離散群と量子トポロジー",
+  //     "venue": "文部科学省共済組合箱根宿泊所"
+  //   }
+  // },
+  {
+    "base": {
+      "date": "2015-11-23T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "有理Gorenstein空間上のストリングトポロジーについて",
+      "conference": "ホモトピー論シンポジウム",
+      "url": "http://www.math.u-ryukyu.ac.jp/~tsukuda/homotopy2015/",
+      "venue": "姫路・西はりま地場産業センター(じばさんびる)"
+    }
+  },
+  {
+    "base": {
+      "date": "2015-12-12T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "有理Gorenstein空間上のストリングトポロジーについて",
+      "conference": "リーマン面の幾何の展開",
+      "venue": "KKR伊豆長岡 千歳荘"
+    }
+  },
+  {
+    "base": {
+      "date": "2016-09-06T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "Description and triviality of the loop products and coproducts for rational Gorenstein spaces",
+      "conference": "リーマン面に関連する位相幾何学",
+      "url": "https://sites.google.com/view/ytadokoro/Topological_Studies_around_Riemann_Surfaces/2016",
+      // "url": "https://www.ms.u-tokyo.ac.jp/~tado/riemann_surface16.html", // broken link
+      "venue": "東京大学"
+    }
+  },
+  {
+    "base": {
+      "date": "2016-09-15T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "String topology on rational Gorenstein spaces",
+      "conference": "日本数学会 秋季総合分科会",
+      "url": "http://mathsoc.jp/meeting/kansai16sept/",
+      "venue": "関西大学"
+    },
+    "en": {
+      "title": "String topology on rational Gorenstein spaces",
+      "conference": "MSJ Autumn Meeting 2016",
+      "url": "http://mathsoc.jp/en/meeting/kansai16sept/",
+      "venue": "Kansai University"
+    }
+  },
+  {
+    "base": {
+      "date": "2016-10-30T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "Computation of string operations using rational homotopy theory",
+      "conference": "トポロジーとコンピュータ 2016",
+      "url": "http://www.math.akita-u.ac.jp/tc2016/",
+      "venue": "秋田市 カレッジプラザ"
+    }
+  },
+  {
+    "base": {
+      "date": "2016-11-30T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "Description and triviality of the loop products and coproducts for rational Gorenstein spaces",
+      "conference": "信州トポロジーセミナー",
+      "url": "http://math.shinshu-u.ac.jp/~topology/seminar/archive16.html",
+      "venue": "信州大学"
+    }
+  },
+  {
+    "base": {
+      "date": "2017-03-24T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "Sullivan 代数の semi-pure 性について",
+      "conference": "日本数学会 2017年度年会",
+      "url": "https://mathsoc.jp/meeting/tmu17mar/",
+      "venue": "首都大学東京"
+    },
+    "en": {
+      "title": "Sullivan 代数の semi-pure 性について",
+      "conference": "MSJ Spring Meeting 2017",
+      "url": "https://mathsoc.jp/en/meeting/tmu17mar/",
+      "venue": "Tokyo Metropolitan University"
+    }
+  },
+  {
+    "base": {
+      "date": "2017-05-22T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "String topology on rational Gorenstein spaces",
+      "conference": "変換群を核とする代数的位相幾何学",
+      "url": "https://www.rs.tus.ac.jp/math2/math2/tsatoh/tsatoh-homepage/rims_trans_2017_program.pdf",
+      "venue": "京都大学数理解析研究所"
+    }
+  },
+  {
+    "base": {
+      "date": "2017-07-06T00:00:00.000Z",
+      "lang": "en"
+    },
+    "en": {
+      "title": "String topology on rational Gorenstein spaces",
+      "conference": "Young Topologists Meeting",
+      "url": "https://www.math-stockholm.se/en/konferenser-och-akti/young-topologists-meeting-2017-1.670396",
+      "venue": "KTH Royal Institute of Technology in Stockholm, Sweden"
+    }
+  },
+  {
+    "base": {
+      "date": "2017-11-24T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "Brane topology on rational Gorenstein spaces",
+      "conference": "ホモトピー論シンポジウム",
+      "url": "http://bach.kinwu.ac.jp/homotopy2017/",
+      "venue": "高松市生涯学習センター(まなびCAN)"
+    }
+  },
+  {
+    "base": {
+      "date": "2017-12-09T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "Coproducts in brane topology",
+      "conference": "ストリングトポロジーとその周辺",
+      "venue": "四季の湯強羅静雲荘"
+    }
+  },
+  {
+    "base": {
+      "date": "2018-08-30T00:00:00.000Z",
+      "lang": "en"
+    },
+    "en": {
+      "title": "Coproducts in brane topology on rational Gorenstein spaces",
+      "conference": "Mapping Spaces in Algebraic Topology",
+      // "url": "https://www.math.kyoto-u.ac.jp/~kishi/conference/kyoto2018/top", // broken link
+      "venue": "Kyoto University"
+    }
+  },
+  {
+    "base": {
+      "date": "2018-09-25T00:00:00.000Z",
+      "lang": "en"
+    },
+    "en": {
+      "title": "Coproducts in brane topology on rational Gorenstein spaces",
+      "conference": "Topological Activities",
+      "url": "https://www.math-stockholm.se/en/kalender/shun-wakatsuki-coproducts-in-brane-topology-on-rational-gorenstein-spaces-1.845240?date=2018-09-25&orgdate=2018-09-01&length=1&orglength=30",
+      "venue": "Stockholm University"
+    }
+  },
+  {
+    "base": {
+      "date": "2018-11-04T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "Properties of brane operations",
+      "conference": "ホモトピー論シンポジウム2018",
+      "url": "https://www.math.kyoto-u.ac.jp/~kishi/conference/tokyo2018.html",
+      "venue": "東京工業大学"
+    }
+  },
+  {
+    "base": {
+      "date": "2019-03-06T00:00:00.000Z",
+      "lang": "en"
+    },
+    "en": {
+      "title": "Gorenstein spaces and string topology",
+      "conference": "Building-up Differential Homotopy Theory",
+      "url": "https://www2.math.kyushu-u.ac.jp/~iwase/BDHT/Home.html",
+      "venue": "Kyushu University"
+    }
+  },
+  {
+    "base": {
+      "date": "2019-03-17T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "Generalization of the loop coproduct",
+      "conference": "日本数学会・2019年度年会",
+      "url": "http://mathsoc.jp/meeting/titech19mar/",
+      "venue": "東京工業大学"
+    },
+    "en": {
+      "title": "Generalization of the loop coproduct",
+      "conference": "MSJ Spring Meeting 2019",
+      "url": "http://mathsoc.jp/en/meeting/titech19mar/",
+      "venue": "Tokyo Institute of Technology"
+    }
+  },
+  {
+    "base": {
+      "date": "2019-06-05T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "Brane coproducts and vanishing of cup products on sphere spaces",
+      "conference": "信州トポロジーセミナー",
+      "url": "http://math.shinshu-u.ac.jp/~topology/seminar/",
+      "venue": "信州大学"
+    }
+  },
+  {
+    "base": {
+      "date": "2019-07-02T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "Brane coproducts and their applications",
+      "conference": "トポロジー火曜セミナー",
+      "url": "http://www.ms.u-tokyo.ac.jp/seminar/topology/index.html",
+      "venue": "東京大学"
+    },
+    "en": {
+      "title": "Brane coproducts and their applications",
+      "conference": "Tuesday Seminar on Topology",
+      "url": "http://www.ms.u-tokyo.ac.jp/seminar/topology_e/index_e.html",
+      "venue": "University of Tokyo"
+    }
+  },
+  {
+    "base": {
+      "date": "2019-08-08T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "Brane coproducts and their applications",
+      "conference": "トポロジーシンポジウム",
+      "url": "http://www.math.akita-u.ac.jp/ts2019/",
+      "venue": "秋田市・にぎわい交流館AU（あう）"
+    }
+  },
+  {
+    "base": {
+      "date": "2020-03-04T00:00:00.000Z",
+      "lang": "en"
+    },
+    "en": {
+      "title": "Differential forms in rational homotopy theory",
+      "conference": "Building-up Differential Homotopy Theory 2020",
+      "url": "https://www2.math.kyushu-u.ac.jp/~iwase/BDHT2/Home.html",
+      "venue": "Shinshu University"
+    }
+  },
+  {
+    "base": {
+      "date": "2020-06-17T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "Vanishing of some cup products on mapping spaces from spheres",
+      "conference": "東工大トポロジーセミナー",
+      "url": "https://sites.google.com/view/titech-topology/home",
+      "venue": "Zoom"
+    }
+  },
+  {
+    "base": {
+      "date": "2021-07-16T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "Vanishing of some cup products on mapping spaces from spheres",
+      "conference": "信州・京都・九大トポロジーセミナー",
+      "url": "http://math.shinshu-u.ac.jp/~topology/seminar/",
+      "venue": "Zoom"
+    }
+  },
+  {
+    "base": {
+      "date": "2021-11-07T00:00:00.000Z",
+      "lang": "ja"
+    },
+    "ja": {
+      "title": "A reduction of the string bracket to the loop product",
+      "conference": "ホモトピー論シンポジウム2021",
+      "url": "https://sites.google.com/view/homotopytheorysymposium2021/home",
+      "venue": "Zoom"
+    }
+  },
+  {
+    "base": {
+      "date": "2021-11-10T00:00:00.000Z",
+      "lang": "en"
+    },
+    "en": {
+      "title": "BV exactness and string brackets",
+      "conference": "Purdue Topology Seminar",
+      "url": "https://sites.google.com/view/purduetopologyseminar",
+      "venue": "Zoom"
+    }
+  }
+]
